18bda3022078cf5d15515c3926f8cc67c6118185
iwdifouta1-byte <iwdifouta1@gmail.com>
2026-06-21 00:48:36 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,37 @@
 
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import shutil
+import uuid
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@app.get("/")
+def home():
+    return {"message": "Convert API is running"}
+
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+
+    file_id = str(uuid.uuid4())
+    file_path = f"{UPLOAD_DIR}/{file_id}_{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "file uploaded",
+        "file_path": file_path
+  }
